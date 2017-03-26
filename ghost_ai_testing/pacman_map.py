1f24fe0aeef736905d766b9319647029435532cc
Stein Robert Olsen <steinrobertolsen@Steins-MacBook-Pro.local>
2017-03-26 13:59:56 +0200

    add pathfinder class

--- ghost_ai_testing/pacman_map.py
+++ ghost_ai_testing/pacman_map.py
@@ -2,7 +2,7 @@ import numpy
 import os 
 import random
 
-class pacman:
+class pacman_map:
 
 	def __init__(self, custom_map = None):
 		if custom_map:
@@ -52,18 +52,42 @@ class pacman:
 	def _init_ghost(self):
 		y = random.choice(range(self._map.shape[0]))
 		x = random.choice(range(self._map.shape[1]))
-		if self._map[x, y] == 0:
+		if self._map[x, y] != 1:
 			return self._init_ghost()
 		print("initializing ghost at {}, {}".format(x, y))
 		self._map[x, y] = 4
 
-	def find_pacman(self):
-		pacman = numpy.where(self._map == 3)
-		ghost = numpy.where(self._map == 4)
+	def map(self):
+		return self._map
+
+class pathfinder:
+
+	def __init__(self, pacman_map):
+		self._map = pacman_map.map()
+		self.open_tiles = []
+		self.closed_tiles = []
+		self._search_adjacent_tiles()
+
+	def _find_pacman(self):
+		return tuple([int(pos) for pos in numpy.where(self._map == 3)])
+
+	def _find_ghost(self):
+		return tuple([int(pos) for pos in numpy.where(self._map == 4)])
+
+	def _search_adjacent_tiles(self):
+		pos = self._find_ghost()
+		up = tuple([pos[0], pos[1] - 1]) if (pos[1] - 1) >= 0 else None
+		right = tuple([pos[0] + 1, pos[1]]) if (pos[0] + 1) >= 0 else None
+		down =  tuple([pos[0], pos[1] + 1]) if (pos[1] + 1) < self._map.shape[0] else None
+		left = tuple([pos[0] - 1, pos[1]]) if (pos[0] - 1) < self._map.shape[1] else None
+		adjacent_tiles = [tile for tile in [up, right, down, left] if self._map[tile] == 1]
+		self.open_tiles = adjacent_tiles
 
 def main():
-	game = pacman("nowalls_map.pacmap")
-	print(game)
+	game = pacman_map("nowalls_map.pacmap")
+	finder = pathfinder(game)
+
+	
 
 if __name__ == "__main__":
 	main()
