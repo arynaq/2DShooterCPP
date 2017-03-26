f65915616388a405da49385079621b23a70d1632
Stein Robert Olsen <steinrobertolsen@Steins-MacBook-Pro.local>
2017-03-26 12:23:35 +0200

    Create map, init pacman and ghost

---
+++ ghost_ai_testing/pacman_map.py
@@ -0,0 +1,64 @@
+import numpy
+import os 
+import random
+
+class pacman:
+
+	def __init__(self, custom_map = None):
+		if custom_map:
+			with open(custom_map, "rb") as f:
+				self._map = numpy.array([list(line) for line in f.read().strip().split()]).astype(int)
+		else:
+			self._map = self._create_default_map()
+		#self._init_pacman()
+		#self._init_ghost()
+
+	def _create_default_map(self):
+		return numpy.array([
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+		[0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+		[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+		[0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+		[0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0],
+		[0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+		[1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+		[0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+		[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+		[0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+		[0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0],
+		[0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+		[0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0],
+		[0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+		[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		])
+
+	def _init_pacman(self):
+		x = random.choice(range(self._map.shape[0]))
+		y = random.choice(range(self._map.shape[1]))
+		if self._map[x, y] != 1:
+			return self._init_pacman()
+		print("initializing pacman at {}, {}".format(x, y))
+		self._map[x, y] = 3
+
+	def _init_ghost(self):
+		y = random.choice(range(self._map.shape[0]))
+		x = random.choice(range(self._map.shape[1]))
+		if self._map[x, y] == 0:
+			return self._init_ghost()
+		print("initializing ghost at {}, {}".format(x, y))
+		self._map[x, y] = 4
+
+	def find_pacman(self):
+		pacman = numpy.where(self._map == 3)
+		ghost = numpy.where(self._map == 4)
+def main():
+	game = pacman("nowalls_map.pacmap")
+
+if __name__ == "__main__":
+	main()
